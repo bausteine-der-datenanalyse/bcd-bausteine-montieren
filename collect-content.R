7d4e02b1517ed70ceaf763528a2ae86e9ac0828c
Matthias Baitsch <matthias.baitsch@me.com>
2024-10-25 07:32:25 +0200

    Refactor collect-content.R continued

--- collect-content.R
+++ collect-content.R
@@ -22,18 +22,6 @@ test_include <- function(path, exclude_patterns) {
     file_test("-f", path)
 }
 
-substitute_definitions <- function(string) {
-    for (key in names(definitions)) string <- str_replace_all(string, key, definitions[[key]])
-    string
-}
-
-make_name <- function(base, part_folder, name, idx) {
-    definitions[["\\$\\{part\\-folder\\}"]] <<- part_folder
-    definitions[["\\$\\{name\\}"]] <<- name
-    definitions[["\\$\\{idx\\}"]] <<- str_pad(idx, 2, side = "left", pad = "0")
-    substitute_definitions(base)
-}
-
 prepare_copy_to <- function(to, overwrite) {
     do_overwrite <- !is.null(overwrite) && overwrite
     if (file.exists(to) && !do_overwrite) stop("File exists: ", to)
@@ -46,7 +34,17 @@ file_copy_safe <- function(from, to, overwrite) {
     file.copy(from, to)
 }
 
-copy_job <- function(from, to, exclude_patterns, overwrite) {
+
+# -------------------------------------------------------------------------------------------------
+# Job functions
+# -------------------------------------------------------------------------------------------------
+
+copy_job <- function(job) {
+    from <- substitute_definitions(job$from)
+    to <- substitute_definitions(job$to)
+    exclude_patterns <- job$`exclude-patterns`
+    overwrite <- job$overwrite
+
     if (file_test("-f", from)) { # Copy one file
         file_copy_safe(from, to, overwrite)
     } else if (file_test("-d", from)) { # Copy folder
@@ -59,79 +57,165 @@ copy_job <- function(from, to, exclude_patterns, overwrite) {
     }
 }
 
-zip_job <- function(zipfile, folder, root) zip::zip(zipfile, folder, root = root)
+zip_job <- function(job) {
+    zipfile <- substitute_definitions(job$zipfile)
+    folder <- substitute_definitions(job$folder)
+    root <- substitute_definitions(job$root)
+    zip::zip(zipfile, folder, root = root)
+}
 
-delete_job <- function(folder) {
+delete_job <- function(job) {
+    folder <- substitute_definitions(job$folder)
     unlink(folder, recursive = TRUE)
 }
 
+# pwp: Path with pattern
+do_assignments <- function(pwps, with_solution) {
+    #
+    # Process files
+    for (pwp in pwps) {
+        path <- substitute_definitions(pwp)
+        folder <- dirname(path)
+        pattern <- basename(path)
+
+        for (file in list.files(path = folder, pattern = pattern)) {
+            file_sol <- str_replace(file, fixed(".qmd"), ".sol.qmd")
+            is_solution <- str_detect(file, fixed(".sol."))
+            have_solution <- file.exists(file.path(folder, file_sol))
+
+            if (!is_solution) {
+                #
+                # Cat assignment
+                cat(read_lines(file.path(folder, file)), sep = "\n")
+                cat("\n")
+
+                # Announce solution
+                if (with_solution) {
+                    cat("### Lösung {-}\n\n")
+                    if (!have_solution) cat("Keine Lösung\n")
+                }
+            } else if (with_solution) {
+                #
+                # Cat solution
+                cat(read_lines(file.path(folder, file)), sep = "\n")
+                cat("\n")
+            }
+        }
+    }
+}
+
+assignment_paper_job <- function(job) {
+    define("sol", job$sol)
+    define("subtitle", job$subtitle)
+
+    sink(substitute_definitions("${target-folder}/aufgabenblatt-${idx}.qmd"))
+    cat(substitute_definitions("${assignment-header}"))
+    cat("\n")
+    do_assignments(job$files, with_solution = FALSE)
+    sink()
+
+    sink(substitute_definitions("${target-folder}/aufgabenblatt-${idx}-loesung-${sol}.qmd"))
+    cat(substitute_definitions("${assignment-solution-header}"))
+    cat("\n")
+    do_assignments(job$files, with_solution = TRUE)
+    sink()
+}
+
+increment_index_job <- function(job) increment_index()
+
+jobs <- list(
+    "copy" = copy_job,
+    "zip" = zip_job,
+    "delete" = delete_job,
+    "assignment-paper" = assignment_paper_job,
+    "increment-index" = increment_index_job
+)
+
+handle_job <- function(job) jobs[[names(job)[[1]]]](job)
+
+
+# -------------------------------------------------------------------------------------------------
+# Definitions and index
+# -------------------------------------------------------------------------------------------------
+
+# Definitions
+definitions <- list()
+
+define <- function(var, val) definitions[[var]] <<- val
+
+substitute_definitions <- function(s) {
+    for (key in names(definitions)) {
+        value <- definitions[[key]]
+        var <- paste0("${", key, "}")
+        if (length(value)) s <- str_replace_all(s, fixed(var), value)
+    }
+    s
+}
+
+# Index
+idx <- 0
+
+reset_index <- function() {
+    idx <<- 0
+    increment_index()
+}
+
+increment_index <- function() {
+    idx <<- idx + 1
+    define("sidx", idx)
+    define("idx", str_pad(idx, 2, side = "left", pad = "0"))
+}
+
+
+# -------------------------------------------------------------------------------------------------
+# Process input
 # -------------------------------------------------------------------------------------------------
 
 
 # Read YAML file
 yaml <- read_yaml("content.yml")
+
 target_folder <- yaml$`target-folder`
 deploy_folder <- here::here(yaml$`deploy-folder`)
 
 
-# Global definitions
-definitions <- list(
-    "\\$\\{target\\-folder\\}" = target_folder,
-    "\\$\\{deploy\\-folder\\}" = deploy_folder
-)
+define("target-folder", target_folder)
+define("deploy-folder", deploy_folder)
+define("project-folder", here::here())
+
+
+for (def in yaml$definitions) {
+    var <- names(def)[1]
+    val <- def[[var]]
+    define(var, val)
+}
 
 
 # Clean up and make target folder
+target_folder <- definitions[["target-folder"]]
 if (dir.exists(target_folder)) unlink(target_folder, recursive = TRUE)
 dir.create(target_folder, recursive = TRUE)
 
 
 # Process jobs
-for (job in yaml$jobs) {
-    keys <- names(job)
-    if (keys[[1]] == "copy") {
-        copy_job(
-            make_name(job$from, "xxx", "yyy", 99),
-            make_name(job$to, "xxx", "yyy", 99),
-            job$`exclude-patterns`,
-            job$overwrite
-        )
-    } else {
-        stop("Unknown job: ", keys[[1]])
-    }
-}
+reset_index()
+walk(yaml$jobs, handle_job)
 
 
-# Parts
-idx <- 1
+# Process parts
+reset_index()
 for (part in yaml$parts) {
-    part_folder <- part$folder
-    part_folder_path <- here::here(part_folder)
-    if (!dir.exists(part_folder_path)) stop("Source folder does not exist: ", part_folder_path)
+    #
+    # Define name and part folder
+    define("name", str_match(part$folder, yaml$`part-name-pattern`)[2])
+    define("part-folder", here::here(part$folder))
 
-    for (job in yaml$`jobs-on-parts`) {
-        keys <- names(job)
-        name <- str_match(part$folder, yaml$`part-name-pattern`)[2]
-        if (keys[[1]] == "copy") {
-            copy_job(
-                make_name(job$from, here::here(part_folder), name, idx),
-                make_name(job$to, part_folder, name, idx),
-                job$`exclude-patterns`,
-                job$overwrite
-            )
-        } else if (keys[[1]] == "zip") {
-            zip_job(
-                make_name(job$zipfile, part_folder, name, idx),
-                make_name(job$folder, part_folder, name, idx),
-                make_name(job$root, part_folder, name, idx)
-            )
-        } else if (keys[[1]] == "delete") {
-            delete_job(
-                make_name(job$folder, here::here(part_folder), name, idx)
-            )
-        } else {
-            stop("Unknown job-on-parts: ", keys[[1]])
-        }
-    }
-    idx <- idx + 1
+    # Check if part folder exists
+    if (!dir.exists(definitions[["part-folder"]])) stop("Part folder does not exist: ", definitions[["part-folder"]])
+
+    # Process jobs
+    walk(yaml$`jobs-on-parts`, handle_job)
+
+    # Increment counter
+    increment_index()
 }
